98b4340df0355548959e6ad992b71b5876655d17
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-04-09 21:59:38 -0300

    Corrigindo resultado do auipc

--- tests/RV32I/basic/src/042-forwarding-lw.s
+++ tests/RV32I/basic/src/042-forwarding-lw.s
@@ -4,13 +4,15 @@ _start:
     addi a1, zero, 10      # Carrega o primeiro número (10) em a1
     addi a2, zero, 20      # Carrega o segundo número (20) em a2
 
-    add a3, a1, a2       # Soma a1 e a2, armazena o resultado em a3
+    add a3, a1, a2       # Soma a1 e a2, armazena o resultado em a3 - 
+                          # a3 = 10 + 20 = 30
     
     sw a3, 40(zero)
 
-    addi a4, zero, 1
+    addi a4, zero, 1     # Carrega o número 1 em a4
 
-    lw a5, 40(zero)
+    lw a5, 40(zero)   # Carrega o valor de a3 (30) em a5 
+    
     add a5, a5, a4
 
     sw a5, 60(zero)
